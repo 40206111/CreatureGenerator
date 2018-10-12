2a8eb927c658e895e3b6f199443e03d22c759758
40206111 <40206111@live.napier.ac.uk>
2018-10-12 16:06:25 +0100

    Storing Creature Values -Create class to store creature parts positions

---
+++ CreatureGenerator/Assets/Scripts/Creature.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature
+{
+
+    //main points of creature
+    public List<Vector3> points = new List<Vector3>();
+
+}
---
+++ CreatureGenerator/Assets/Scripts/Gm.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gm : MonoBehaviour {
+
+    private Creature c1;
+    private GameObject creature2;
+    private GameObject creature3;
+
+    private void Creature1()
+    {
+        c1 = new Creature();
+        c1.points.Add(new Vector3(0.0f, 0.0f, 0.0f));
+        c1.points.Add(new Vector3(0.0f, 1.0f, 0.0f));
+    }
+
+    // Use this for initialization
+    void Start () {
+        Creature1();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDrawGizmos()
+    {
+        //check if points exsist
+        if (c1 == null)
+        {
+            return;
+        }
+        //change gizmo colour to black
+        Gizmos.color = Color.cyan;
+        //draw points of creature
+        for (int i = 0; i < c1.points.Count; i++)
+        {
+            Gizmos.DrawSphere(c1.points[i], 0.1f);
+        }
+    }
+}
